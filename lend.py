835dc7e598f2a6e87aebe63087316faf68b74be2
Leo Liu <changtong1993@gmail.com>
2014-02-13 16:32:25 +0800

    main app init

---
+++ lend.py
@@ -0,0 +1,25 @@
+import os
+import tornado.ioloop
+import tornado.web
+
+settings = {
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "debug": True,
+}
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        items = ["Leo_c_t", "Tom"]
+        self.render("index.html", title="Sound Like Hot", name=items[0])
+
+# route
+application = tornado.web.Application([
+    (r"/", IndexHandler),
+], **settings
+)
+
+if __name__ == "__main__":
+    application.listen(8002)
+    tornado.ioloop.IOLoop.instance().start()
