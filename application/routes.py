4fbd5489c713b61f7e946cf372e3be8d07e0e758
Leo Liu <changtong1993@gmail.com>
2014-02-22 14:08:39 +0800

    pep8 style

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,10 @@
 #coding=utf8
 
-from controllers import IndexHandler, LoanHandler, HistoryHandler, LoginHandler
+from controllers import \
+    IndexHandler, \
+    LoanHandler, \
+    HistoryHandler, \
+    LoginHandler
 
 handlers = [
     (r"/loan/([0-9]+)", LoanHandler),
