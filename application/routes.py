0d76a4681ce4fce43e95c0683273e3d7e282fdb1
Leo Liu <changtong1993@gmail.com>
2014-03-09 15:15:33 +0800

    add 4 new routes.

--- application/routes.py
+++ application/routes.py
@@ -4,11 +4,21 @@ from controllers import \
     IndexHandler, \
     LoanHandler, \
     HistoryHandler, \
-    LoginHandler
+    LoginHandler, \
+    LogoutHandler, \
+    RegisterHandler, \
+    LoanRequestHandler, \
+    DueRequestHandler, \
+    SplitRequestHandler
 
 handlers = [
     (r"/loan/([0-9]+)", LoanHandler),
     (r"/history/([0-9]+)", HistoryHandler),
     (r"/login", LoginHandler),
+    (r"/logout", LogoutHandler),
+    (r"/register", RegisterHandler),
+    (r"/loan_request", LoanRequestHandler),
+    (r"/due_request", DueRequestHandler),
+    (r"/split_request", SplitRequestHandler),
     (r"/", IndexHandler),
 ]
