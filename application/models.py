0814cb26a20dae1e44b8bbccf6453e3fed62814f
Leo Liu <changtong1993@gmail.com>
2014-03-23 22:03:51 +0800

    autocommit=True and comment all session.commit()

--- application/models.py
+++ application/models.py
@@ -28,13 +28,13 @@ class UserModel(BaseModel):
                         password=user['password'],
                         phone=user['phone'])
         session.add(new_user)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def delete_user(user_id):
         user = session.query(User).filter(User.user_id == user_id)
         session.delete(user)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_user(user):
@@ -71,7 +71,7 @@ class UserModel(BaseModel):
         up_user.pic3 = user['pic3']
         up_user.pic4 = user['pic4']
         up_user.status = 1
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_user_info(user):
@@ -97,7 +97,7 @@ class UserModel(BaseModel):
         up_user.major = user['major']
         up_user.student_id = user['student_id']
         up_user.dorm = user['dorm']
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_user_password(user_id, password):
@@ -107,7 +107,7 @@ class UserModel(BaseModel):
         # )
         up_user = session.query(User).filter(User.user_id == user_id).one()
         up_user.password = password
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_user_avatar(user):
@@ -118,20 +118,20 @@ class UserModel(BaseModel):
         up_user = session.query(User).filter(User.user_id == user['user_id']) \
             .one()
         up_user.avatar = user['avatar']
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_user_status(user_id):
         # change user status to 1 完善资料之后
         up_user = session.query(User).filter(User.user_id == user_id).one()
         up_user.status = 1
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_user_credit(user_id, credit):
         user = session.query(User).filter(User.user_id == user_id).one()
         user.credit = int(credit)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def get_user_info(user_id):
@@ -183,7 +183,7 @@ class GuaranteeModel(BaseModel):
                                   warrantee_id=guarantee['warrantee_id'],
                                   status=0)
         session.add(new_guarantee)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def delete_guarantee(guarantor_id, warrantee_id):
@@ -191,7 +191,7 @@ class GuaranteeModel(BaseModel):
             filter(Guarantee.guarantor_id == guarantor_id).\
             filter(Guarantee.warrantee_id == warrantee_id).one()
         session.delete(guarantee)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def change_status(guarantee_id):
@@ -199,7 +199,7 @@ class GuaranteeModel(BaseModel):
         guarantee = session.query(Guarantee). \
             filter(Guarantee.guarantee_id == guarantee_id).one()
         guarantee.status = 1
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def get_user_guarantor(user_id):
@@ -288,13 +288,13 @@ class LoanModel(BaseModel):
                         due_status=0,
                         check_status=0)
         session.add(new_loan)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def delete_loan(loan_id):
         loan = session.query(Loan).filter(Loan.loan_id == loan_id).one()
         session.delete(loan)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def update_loan(loan):
@@ -306,14 +306,14 @@ class LoanModel(BaseModel):
         up_loan.remain_amount = loan['remain_amount']
         up_loan.due_date = loan['due_date']
         up_loan.split_status = 1
-        session.commit()
+        # session.commit()
         return True
 
     @staticmethod
     def update_remain_amount(loan_id, remain_amount):
         loan = session.query(Loan).filter(Loan.loan_id == loan_id).one()
         loan.remain_amount = remain_amount
-        session.commit()
+        # session.commit()
 
     # @staticmethod
     # def update_due_date(loan_id, due_date):
@@ -327,7 +327,7 @@ class LoanModel(BaseModel):
         loan.due_status = int(status)
         loan.due_date = due_date
         loan.remain_amount = remain_amount
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def change_split_status(loan_id, status):
@@ -335,7 +335,7 @@ class LoanModel(BaseModel):
         #         1 分期
         loan = session.query(Loan).filter(Loan.loan_id == loan_id).one()
         loan.split_status = int(status)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def change_check_status(loan_id, status):
@@ -344,7 +344,7 @@ class LoanModel(BaseModel):
         #         2 complete
         loan = session.query(Loan).filter(Loan.loan_id == loan_id).one()
         loan.check_status = int(status)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def get_loan_info(loan_id):
@@ -451,21 +451,21 @@ class BehaviourModel(BaseModel):
                                   time=behaviour['time'],
                                   check_status=behaviour['check_status'])
         session.add(new_behaviour)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def delete_behaviour(behaviour_id):
         behaviour = session.query(Behaviour).\
             filter(Behaviour.behaviour_id == behaviour_id).one()
         session.delete(behaviour)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def change_status(behaviour_id, status):
         behaviour = session.query(Behaviour). \
             filter(Behaviour.behaviour_id == behaviour_id).one()
         behaviour.check_status = int(status)
-        session.commit()
+        # session.commit()
         
     def get_user_all_behaviours(self, user_id):
         try:
@@ -509,7 +509,7 @@ class SplitLoanModel(BaseModel):
                               amount_per=split['amount_per'],
                               next_date=split['next_date'])
         session.add(new_split)
-        session.commit()
+        # session.commit()
 
     @staticmethod
     def get_split_info(loan_id):
@@ -530,7 +530,7 @@ class SplitLoanModel(BaseModel):
             return False
 
         split.next_date = next_date
-        session.commit()
+        # session.commit()
 
 
 class PayModel(BaseModel):
@@ -542,7 +542,7 @@ class PayModel(BaseModel):
                       check_status=pay['check_status'],
                       date=pay['date'])
         session.add(new_pay)
-        session.commit()
+        # session.commit()
 
     def update_check_status(self, pay_id):
         try:
@@ -550,7 +550,7 @@ class PayModel(BaseModel):
         except NoResultFound:
             return False
         pay.check_status = 1
-        session.commit()
+        # session.commit()
         return True
 
     def get_pay_info(self, loan_id):
