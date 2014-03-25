a2b8ddd4ac8533bee026963eb2151a146484c126
Leo Liu <changtong1993@gmail.com>
2014-03-25 22:04:22 +0800

    type 7 to guarantee_request.

--- application/controllers.py
+++ application/controllers.py
@@ -816,7 +816,7 @@ class GuaranteeRequestHandler(BaseHandler):
         # todo 两人已建立担保关，告诉路少德
         if self.guarantee_model.check_guarantee_exist(guarantor_id, user['user_id']):
             # 两人已建立担保关系
-            result_json = json.dumps({'result': 6},
+            result_json = json.dumps({'result': 7},
                                      separators=(',', ':'),
                                      encoding="utf-8", indent=4,
                                      ensure_ascii=False)
